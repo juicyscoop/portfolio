261a2ef705ad02ad254dd498285d919212a16361
Oskar <oskar.marelja@semantic-visions.com>
2023-12-12 20:44:51 +0100

    update about page

--- src/components/About.js
+++ src/components/About.js
@@ -20,7 +20,7 @@ export default function About() {
               Work With Me
             </a>
             <a
-              href="https://github.com/juicyscoop?tab=repositories"
+              href="https://github.com/juicyscoop"
               className="ml-4 inline-flex text-gray-200 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
               See My Work
             </a>
